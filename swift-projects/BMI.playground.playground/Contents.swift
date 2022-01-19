1ee354b3dd4eeac6ef768b0a614240ee5a2b646b
ali-fathi <fathialo81@gmail.com>
2022-01-19 00:20:18 -0700

    Completed BMI in swift

---
+++ swift-projects/BMI.playground.playground/Contents.swift
@@ -0,0 +1,12 @@
+// BMI Calculator
+
+// height in meter
+let height = 1.73
+// weight in kg
+let weight = 82.0
+
+let bmi = Int((weight / (height * height)))
+print("Your BMI is \(bmi)")
+
+let myString = "Hello"
+myString.count
