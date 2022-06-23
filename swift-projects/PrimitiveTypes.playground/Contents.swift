82e0d5abcdda0bf7eeaff1aa5c771adcc4a68ec2
ali-fathi <fathialo81@gmail.com>
2022-06-22 21:51:17 -0600

    add primitive data types files to the swift project

---
+++ swift-projects/PrimitiveTypes.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+// Primitve Types in Swift
+
+// Integer
+let age: Int = 32
+
+// String
+let name: String = "Jim"
+
+// Float
+let height: Float = 1.83
+
+// Double
+let moneyInThePocket: Double = 20.0
+print("My name is \(name), I am \(age) years old. I am about \(height) meters tall and I have $\(moneyInThePocket) in my pocket")
+
