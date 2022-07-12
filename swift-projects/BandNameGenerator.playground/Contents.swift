a86b8a9dd0a9c4c511c89e6f67419a4b6ed0e4f6
ali-fathi <fathialo81@gmail.com>
2022-07-12 02:03:29 -0600

    add BandNameGenerator in swift

---
+++ swift-projects/BandNameGenerator.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+// TODO 1: Program Greeing
+print("Welcome to the Band Name Generator!")
+
+// TODO 2: Ask for the name of the city they grew up in
+let city = "The six"
+
+// TODO 3: Ask for the name of thier pet
+let pet = "Toronto Band"
+
+// TODO 4: Combine both city and pet as the band name
+print("Your band name could be: \(city) \(pet)")
